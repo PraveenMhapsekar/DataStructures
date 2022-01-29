7af15f0ac8ef3cbca975491990abf34bd43e5f6a
Praveen <Praveen Mhapsekar>
2022-01-28 22:31:48 -0800

    Added some Trie's

---
+++ EDUCATIVE_IO/Trie/Trie.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define CHAR_SIZE 256
+#if 0
+class TrieNode {
+  public:
+    TrieNode *children[CHAR_SIZE];
+    bool isEndWord;
+    TrieNode();
+    void markAsLeaf();
+    void unMarkAsLeaf();
+};
+#endif
+class Trie {
+  public:
+    //TrieNode *root;
+
+    Trie *children[CHAR_SIZE];
+    bool isEndWord;
+   // TrieNode();
+    void markAsLeaf();
+    void unMarkAsLeaf();
+
+  public:
+    Trie() {
+			this->isEndWord = false;
+			for (int i = 0; i < CHAR_SIZE; i++) {
+				this->children[i] = nullptr;
+			}
+    }
+
+    Trie *getRoot() {
+      Trie *root;
+      root = this;
+      return root;
+    }
+
+    int getIndex(char t);
+
+    void insertNode(string key) {
+      Trie *curr = this;
+      for (int i = 0; i < key.length(); i++) {
+        if (curr->children[key[i]] == nullptr) {
+          curr->children[key[i]] = new Trie();
+        }
+        curr = curr->children[key[i]];
+      }
+      curr->isEndWord = true;
+    }
+
+    
+    bool searchNode(string key) {
+      return true;
+    }
+
+    bool hasNoChildren(Trie * currentNode);
+    bool deleteHelper(string key, Trie * currentNode, int length, int level);
+    void deleteNode(string key);
+};
---
+++ EDUCATIVE_IO/Trie/Trie_1_num_of_words.cc
@@ -0,0 +1,31 @@
+#include "Trie.h"
+
+int totalWords(Trie *root){
+  int result = 0;
+
+  // Leaf denotes end of a word
+  if (root->isEndWord)
+    result++;
+
+  for (int i = 0; i < CHAR_SIZE; i++) {
+    if (root->children[i] != nullptr)
+      result += totalWords(root->children[i]);
+  }
+  return result;
+}
+
+
+int
+main() {
+  string keys[9] = {"the", "a", "there", "answer", "any", "by", "bye", "their","abc"};
+
+  Trie *t = new Trie();
+  // Construct trie       
+  for (int i = 0; i < 9 ; i++)
+  {
+    t->insertNode(keys[i]);
+  }
+
+ cout << "total Words " << totalWords(t->getRoot()) << endl;
+  return 0;
+}
