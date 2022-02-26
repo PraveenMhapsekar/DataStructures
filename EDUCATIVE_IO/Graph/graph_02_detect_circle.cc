8e8999aa2ac8a54fb0ef55ca2d14d2056d0c2524
Praveen <Praveen Mhapsekar>
2022-02-26 00:16:24 -0800

    modified graph

--- EDUCATIVE_IO/Graph/graph_02_detect_circle.cc
+++ EDUCATIVE_IO/Graph/graph_02_detect_circle.cc
@@ -1,51 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
-#define NUM 6
-
 bool
-dfs(int isConnected[NUM][NUM], int size, int *visited, int i) {
-  visited[i] = 1;
+dfs(vector<vector<int>> &graph, int size, bool *visited, int i) {
+  visited[i] = true;
   // traverse each column
   for (int j = 0; j < size; j++) {
     // find if circle is detected ?
-    if (isConnected[i][j] == 1) {
-      if (visited[j] == 1) { 
+    if (graph[i][j] == 1) {  // there exist path between i and j vertices
+      if (visited[j] == true) { // vertex j is already visited
         return true;
 			} else {
         // go to corrosponding row and traverse recursively
-        return dfs(isConnected, size, visited, j);
+        return dfs(graph, size, visited, j);
       }
     }
   }
-
   return false;
 }
 
 bool
-findLoop(int isConnected[NUM][NUM], int isConnectedSize, int isConnectedColSize){
-  int count = 0; 
-  int visited[isConnectedSize];
-  
-  if (isConnectedSize != isConnectedColSize) 
-    return count;
-  
+findLoop(vector<vector<int>> &graph) {
+  int graphSize = graph[0].size();  
+  bool visited[graphSize];
+
   // init visited array
-  for (int i = 0; i < isConnectedSize; i++) {
-    visited[i] = 0;
+  for (int i = 0; i < graphSize; i++) {
+    visited[i] = false;
   }
  
    // traverse graph in DFS, row by row
-  for (int i = 0; i < isConnectedSize; i++) {
-    if (visited[i] == 0) {
-      if (dfs(isConnected, isConnectedSize, visited, i) == true) {
-         return true;
+  for (int i = 0; i < graphSize; i++) {
+    if (visited[i] == false) {
+      if (dfs(graph, graphSize, visited, i) == true) {
+        return true; // Found loop return true
       }
-      count++;
     }
   }
   return false;
@@ -53,12 +44,23 @@ findLoop(int isConnected[NUM][NUM], int isConnectedSize, int isConnectedColSize)
 
 int
 main() {
-  int graph[NUM][NUM] = {{0, 1, 0, 0, 0, 0},
-											   {0, 0, 1, 0, 0, 0},
-											   {0, 0, 0, 1, 0, 0},
-											   {0, 0, 0, 0, 1, 0},
-											   {0, 0, 0, 0, 0, 1},
-											   {1, 0, 0, 0, 0, 0}};
-  printf("Loop %s\n", findLoop(graph, NUM, NUM) == true ? "detected" : "not detected");
+  vector<vector<int>> graph = {{0, 1, 0, 0, 0, 0}, // vertex 0 -> 1
+															 {0, 0, 1, 0, 0, 0}, // vertex 1 -> 2
+															 {0, 0, 0, 1, 0, 0}, // vertex 2 -> 3
+															 {0, 0, 0, 0, 1, 0}, // vertex 3 -> 4
+															 {0, 0, 0, 0, 0, 1}, // vertex 4 -> 5
+															 {1, 0, 0, 0, 0, 0}};// vertex 5 -> 0 // loop
+
+  printf("Loop %s\n", findLoop(graph) == true ? "detected" : "not detected");
+
+											graph = {{0, 1, 0, 0, 0, 0}, // vertex 0 -> 1
+															 {0, 0, 1, 0, 0, 0}, // vertex 1 -> 2
+															 {0, 0, 0, 1, 0, 0}, // vertex 2 -> 3
+															 {0, 0, 0, 0, 1, 0}, // vertex 3 -> 4
+															 {0, 0, 0, 0, 0, 1}, // vertex 4 -> 5
+															 {0, 0, 0, 0, 0, 0}};// vertex 5 ->  // no loop
+
+  printf("Loop %s\n", findLoop(graph) == true ? "detected" : "not detected");
+ 
   return 0;
 }
--- EDUCATIVE_IO/Graph/graph_03_mother_vertex.cc
+++ EDUCATIVE_IO/Graph/graph_03_mother_vertex.cc
@@ -8,74 +8,67 @@ We need to find all the mother vertices in the given graph.
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
-#define NUM 4
-
 void
-dfs(int isConnected[NUM][NUM], int size, int *visited, int i) {
+dfs(vector<vector<int>> graph, int size, bool *visited, int i) {
   // traverse each column
   visited[i] = 1;
   for (int j = 0; j < size; j++) {
-    if (isConnected[i][j] == 1 && visited[j] == 0) {
+    if (graph[i][j] == 1 && visited[j] == 0) {
       // go to corrosponding row and traverse recursively
-      dfs(isConnected, size, visited, j );
+      dfs(graph, size, visited, j);
     }
   }
 }
 
-bool
-findMotherVertex(int isConnected[NUM][NUM], int isConnectedSize, int isConnectedColSize) {
-  int count = 0; 
-  int visited[isConnectedSize];
-  
-  if (isConnectedSize != isConnectedColSize) 
-    return count;
-  
+int
+findMotherVertex(vector<vector<int>> graph) {
+  int size = graph[0].size();
+  bool visited[size];
+  int last_i;
+
   // init visited array
-  for (int i = 0; i < isConnectedSize; i++) {
-    visited[i] = 0;
+  for (int i = 0; i < size; i++) {
+    visited[i] = false;
   }
 
-  int last_i; 
-
   // traverse graph in DFS, row by row
-  for (int i = 0; i < isConnectedSize; i++) {
-    if (visited[i] == 0) {
-      dfs(isConnected, isConnectedSize, visited, i);
+  for (int i = 0; i < size; i++) {
+    if (visited[i] == false) {
+      dfs(graph, size, visited, i);
       last_i = i; // last visited vertex
-      count++;
     }
   }
 
   // Clear visited array
-  for (int i = 0; i < isConnectedSize; i++) {
-    visited[i] = 0;
+  for (int i = 0; i < size; i++) {
+    visited[i] = false;
   }
 
   // DFS from last vertex
-  dfs(isConnected, isConnectedSize, visited, last_i);
+  dfs(graph, size, visited, last_i);
 
   // Verify if all nodes are visited, if yes last vertext is mother vertex
-  bool flag = true;
-  for (int i = 0; i < isConnectedSize; i++) {
-		if (visited[i] != 1) {
-		  flag = false;
+  for (int i = 0; i < size; i++) {
+		if (visited[i] == false) {
+		  return -1;
 		}
   } 
 
-  return flag;
+  // All nodes are visited from last_i, this is the mother
+  return last_i;
 }
 
 int
 main() {
   // 0->1->2 3->4 5 
-  int graph[NUM][NUM] = {{1, 1, 0, 0},  // 0 -->0 1
-											   {0, 1, 1, 0},  // 1 -->1 2
-											   {0, 0, 1, 0},  // 2 -->2
-											   {1, 1, 0, 1}}; // 3 -->0 1 3
-  printf("Mother Vertex? %s\n", findMotherVertex(graph, NUM, NUM) == true ? "yes" : "no");
+  vector<vector<int>> graph = {{1, 1, 0, 0},  // 0 -->0 1
+															 {0, 1, 1, 0},  // 1 -->1 2
+															 {0, 0, 1, 0},  // 2 -->2
+															 {1, 1, 0, 1}}; // 3 -->0 1 3
+  printf("Mother Vertex : %d\n", findMotherVertex(graph) );
   return 0;
 }
