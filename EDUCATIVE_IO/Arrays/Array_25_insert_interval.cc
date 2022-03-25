f8281d25eba9ff4db8bf0cc03b0b9459b55038be
Praveen <Praveen Mhapsekar>
2022-03-24 22:39:16 -0700

    Linkedlist Delete All

--- EDUCATIVE_IO/Arrays/Array_25_insert_interval.cc
+++ EDUCATIVE_IO/Arrays/Array_25_insert_interval.cc
@@ -2,12 +2,78 @@
 Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
 */
 
-#1 Add new inteval to the list of intervals
-#2 Sort list of intervals
-#3 merge above list
+using namespace std;
 
- or
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-#1 Add all intervals smaller than newInterval to the list
-#2 merge newInterval with list above
-#3 Try merging remainin
+class Interval {
+ public:
+  int start = 0;
+  int end = 0;
+
+  Interval(int start, int end) {
+    this->start = start;
+    this->end = end;
+  }
+};
+
+class MergeIntervals {
+ public:
+  static vector<Interval> merge(vector<Interval> &intervals) {
+    if (intervals.size() < 2) {
+      return intervals;
+    }
+
+    // sort the intervals by start time
+    sort(intervals.begin(), intervals.end(),
+         [](const Interval &x, const Interval &y) { return x.start < y.start; });
+
+    vector<Interval> mergedIntervals;
+
+    vector<Interval>::const_iterator intervalItr = intervals.begin();
+
+    Interval interval = *intervalItr++;
+
+    int start = interval.start;
+    int end = interval.end;
+
+    while (intervalItr != intervals.end()) {
+      interval = *intervalItr++;
+      if (interval.start <= end) {  // overlapping intervals, adjust the 'end'
+        end = max(interval.end, end);
+      } else {  // non-overlapping interval, add the previous interval and reset
+        mergedIntervals.push_back({start, end});
+        start = interval.start;
+        end = interval.end;
+      }
+    }
+    // add the last interval
+    mergedIntervals.push_back({start, end});
+    return mergedIntervals;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<Interval> input = {{1, 3}, {2, 5}, {7, 9}};
+  cout << "Merged intervals: ";
+  for (auto interval : MergeIntervals::merge(input)) {
+    cout << "[" << interval.start << "," << interval.end << "] ";
+  }
+  cout << endl;
+
+  input = {{6, 7}, {2, 4}, {5, 9}};
+  cout << "Merged intervals: ";
+  for (auto interval : MergeIntervals::merge(input)) {
+    cout << "[" << interval.start << "," << interval.end << "] ";
+  }
+  cout << endl;
+
+  input = {{1, 4}, {2, 6}, {3, 5}};
+  cout << "Merged intervals: ";
+  for (auto interval : MergeIntervals::merge(input)) {
+    cout << "[" << interval.start << "," << interval.end << "] ";
+  }
+  cout << endl;
+}
